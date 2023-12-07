b53ade3a979009d1fa74b0bb58974f09a4bd36e4
Humbe_ko33 <Humbe_ko33@STHQ0122-03>
2023-12-07 17:56:24 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.PortableExecutable;
+
+class Car1
+{
+    public void OperationA()
+    {
+    }
+}
+
+
+class Car2
+{
+    public void OperationB()
+    {
+    }
+}
+
+class Car3
+{
+    public void OperationC()
+    {
+
+    }
+}
+
+class Facade
+{
+    private Car1 car1;
+    private Car2 car2;
+    private Car3 car3;
+
+    public Facade()
+    {
+        car1 = new Car1();
+        car2 = new Car2();
+        car3 = new Car3();
+    }
+
+    public void Operation()
+    {
+        car1.OperationA();
+        car2.OperationB();
+        car3.OperationC();
+    }
+}
